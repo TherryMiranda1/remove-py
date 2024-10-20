6a7f9d9b601b5fcd4140a201a30806a33a7944b3
laptop haru <therrymiranda1@gmail.com>
2024-10-20 11:35:58 +0100

    suport url

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, send_file, render_template
 import logging
 import os
+import requests
+from io import BytesIO
 from PIL import Image
 from rembg import remove
 
@@ -33,6 +35,21 @@ def save_uploaded_file(uploaded_file, upload_dir=UPLOAD_FOLDER):
     except Exception as e:
         logger.error(f"Error al guardar el archivo: {e}")
         raise e
+    
+def download_image_from_url(url):
+    """Descarga una imagen desde una URL y la guarda temporalmente."""
+    try:
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            img = Image.open(BytesIO(response.content))
+            file_path = os.path.join('/tmp', 'downloaded_image.png')
+            img.save(file_path)
+            return file_path
+        else:
+            raise Exception(f"Error al descargar la imagen. Código de estado: {response.status_code}")
+    except Exception as e:
+        logger.error(f"No se pudo descargar la imagen desde la URL: {e}")
+        raise
 
 def remove_background(image_path):
     output_image_path = os.path.splitext(image_path)[0] + '_rmbg.png'
@@ -51,24 +68,31 @@ def visit():
     logger.info("App visitada")
     return render_template('welcome.html')
 
-@app.route('/health', methods=['GET'])
-def health_check():
-    logger.info("Endpoint de salud consultado.")
-    return jsonify({"message": "La API está funcionando correctamente."})
-
 @app.route('/remove-background/', methods=['POST'])
 def remove_bg_api():
-    if 'file' not in request.files:
-        return jsonify({"error": "No se encontró ningún archivo en la solicitud."}), 400
+    
+    if 'file' in request.files:
+        file = request.files['file']
+        logger.info(f"Solicitud recibida: {file.filename}")
 
-    file = request.files['file']
-    logger.info(f"Solicitud recibida: {file.filename}")
+        try:
+            file_path = save_uploaded_file(file)
+        except Exception as e:
+            logger.error(f"No se pudo guardar el archivo: {e}")
+            return jsonify({"error": "No se pudo guardar el archivo: " + str(e)}), 500
 
-    try:
-        file_path = save_uploaded_file(file)
-    except Exception as e:
-        logger.error(f"No se pudo guardar el archivo: {e}")
-        return jsonify({"error": "No se pudo guardar el archivo: " + str(e)}), 500
+    elif 'url' in request.form:
+        image_url = request.form['url']
+        logger.info(f"Solicitud recibida con URL: {image_url}")
+        
+        try:
+            file_path = download_image_from_url(image_url)
+        except Exception as e:
+            logger.error(f"No se pudo descargar la imagen desde la URL: {e}")
+            return jsonify({"error": "No se pudo descargar la imagen desde la URL: " + str(e)}), 500
+
+    else:
+        return jsonify({"error": "No se encontró ningún archivo o URL en la solicitud."}), 400
 
     output_image_path = remove_background(file_path)
 
